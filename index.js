b1e4ff1eb50dca11b416ff66091fa6d170291049
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-06-27 11:10:31 -0700

    Completed Inserting DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+
+const header = document.getElementById('h1');
+newHeader.innerHTML = "<h1>Victory</h1><h3>Bay, is the champion</h3>"
